6c911f6bfbdb7e66201ca86a698ef8072df71ac8
jpcastro14 <castrovoip@gmail.com>
2024-06-06 19:39:15 -0300

    Creating and componentizing elements for the EventForm structure

--- wlaudio/src/components/EventForm/Input/index.jsx
+++ wlaudio/src/components/EventForm/Input/index.jsx
@@ -1,17 +1,17 @@
-import { InputContainer } from "./styles";
+import { InputContainer, Label, Input } from "./styles";
 import React from "react";
 
 function PInput({ text, type, name, placeholder, onChange, value }) {
   return (
     <InputContainer>
-      <label>{text}</label>
-      <input
+      <Label>{text}</Label>
+      <Input
         type={type}
         name={name}
         placeholder={placeholder}
         onChange={onChange}
         value={value}
-      ></input>
+      ></Input>
     </InputContainer>
   );
 }
--- wlaudio/src/components/EventForm/Input/styles.js
+++ wlaudio/src/components/EventForm/Input/styles.js
@@ -2,6 +2,13 @@ import styled from "styled-components";
 import React from "react";
 
 export const InputContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+`;
+
+export const Input = styled.input`
   font-family: poppins;
   font-size: 15px;
   height: 44px;
@@ -9,5 +16,21 @@ export const InputContainer = styled.div`
   border: 1px solid #ccc;
   margin: 5px;
   flex-grow: 1;
+  padding-left: 15px;
+`;
+
+export const DateInput = styled.input`
+  font-family: poppins;
+  font-size: 15px;
+  height: 44px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  margin: 5px;
+  flex-grow: 1;
+  padding-left: 15px;
+`;
+
+export const Label = styled.label`
+  font-size: 18px;
   padding-left: 15px;
 `;
