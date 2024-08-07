6433361a465f2527d4ed3030dc02fabcbbcb8884
jpcastro14 <castrovoip@gmail.com>
2024-08-07 17:17:48 -0300

    Testing and implementing POST requests to the backend

--- wlaudio/src/components/CourseForm/index.jsx
+++ wlaudio/src/components/CourseForm/index.jsx
@@ -1,10 +1,35 @@
 import React, { useState, useEffect } from "react";
 import { A, Container } from "./styles";
+import axios from "axios";
 
 function CourseForm() {
   const [courses, SetCourses] = useState([]);
 
-  useEffect(() => {
+  const [postCourse, SetpostCourse] = useState({
+    title: "",
+    url: "",
+  });
+
+  const handleType = (e) => {
+    const { name, value } = e.target;
+
+    SetpostCourse((postCourse) => {
+      return { ...postCourse, [name]: value };
+    });
+    console.log(postCourse);
+  };
+
+  const handlePost = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("http://localhost:8000/api/v2/courses/", postCourse)
+      .then((res) => console.log(res));
+
+    console.log(postCourse);
+  };
+
+  /*   useEffect(() => {
     const getData = async () => {
       const response = await fetch("http://localhost:8000/api/v2/courses/");
       const result = await response.json();
@@ -12,13 +37,25 @@ function CourseForm() {
     };
 
     getData();
-  }, []);
+  }, []); */
 
   return (
     <Container>
-      {courses.map((item) => (
-        <li>{item.url}</li>
-      ))}
+      <form onSubmit={handlePost}>
+        <input
+          type="text"
+          name="title"
+          value={postCourse.title}
+          onChange={handleType}
+        />
+        <input
+          type="text"
+          name="url"
+          value={postCourse.url}
+          onChange={handleType}
+        />
+        <input type="submit" />
+      </form>
     </Container>
   );
 }
