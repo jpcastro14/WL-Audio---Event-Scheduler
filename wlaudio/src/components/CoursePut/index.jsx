f347b36dcc041032c9c512f63453f85a715c8f80
jpcastro14 <castrovoip@gmail.com>
2024-08-08 19:11:02 -0300

    Testing PUT HTTP requests to already existent courses

--- wlaudio/src/components/CoursePut/index.jsx
+++ wlaudio/src/components/CoursePut/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container } from "./styles";
+import { Container, Input } from "./styles";
 import axios from "axios";
 
 function CourseForm() {
@@ -8,6 +8,7 @@ function CourseForm() {
   const [putCourse, SetputCourse] = useState({
     title: "",
     url: "",
+    ratings: [" "],
   });
 
   const handleType = (e) => {
@@ -21,11 +22,14 @@ function CourseForm() {
 
   useEffect(() => {
     const getData = async () => {
-      const response = await axios.get(
-        "http://localhost:8000/api/v2/courses/2/"
-      );
-
-      SetCourses(response.data);
+      const response = await axios
+        .get("http://localhost:8000/api/v2/courses/1/")
+        .catch((err) => console.log("Erro ocorrido", "=>", err.message));
+      {
+        response
+          ? SetCourses(response.data)
+          : console.log("Não foi possível recuperar os dados");
+      }
     };
     getData();
     console.log(courses);
@@ -54,19 +58,19 @@ function CourseForm() {
   return (
     <Container>
       <form onSubmit={handlePost}>
-        <input
+        <Input
           type="text"
           name="title"
           defaultValue={courses.title}
           onChange={handleType}
         />
-        <input
+        <Input
           type="text"
           name="url"
           defaultValue={courses.url}
           onChange={handleType}
         />
-        <input type="submit" />
+        <Input type="submit" />
       </form>
     </Container>
   );
--- wlaudio/src/components/CoursePut/styles.js
+++ wlaudio/src/components/CoursePut/styles.js
@@ -3,4 +3,10 @@ import styled from "styled-components";
 export const Container = styled.div`
   border-radius: 6px;
   background-color: blue;
+  width: 30%;
+  padding: 30px;
+`;
+
+export const Input = styled.input`
+  width: 100%;
 `;
