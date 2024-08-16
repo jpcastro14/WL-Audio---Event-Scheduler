def551dd52af4abfb87c28575e3e0e8355c9e67d
jpcastro14 <castrovoip@gmail.com>
2024-08-16 19:06:09 -0300

    Correcting courset object declaration

--- wlaudio/src/components/CourseSet/index.jsx
+++ wlaudio/src/components/CourseSet/index.jsx
@@ -6,49 +6,35 @@ import axios from "axios";
 
 function CourseSet() {
   const { id } = useParams();
-  const [course, setCourse] = useState({
-    title: "",
-    url: "",
-    ratings: [" "],
-  });
-  const [ratings, setRatings] = useState([]);
-
-  /*   useEffect(() => {
-    const getdata = async () => {
-      const response = await axios.get(
-        `http://localhost:8000/api/v2/courses/${id}/`
-      );
-      const courses = await response.json();
-
-      setCourse(courses);
-      console.log(course);
-    };
-    getdata();
-  }, []); */
+  const [course, setCourse] = useState({});
 
   useEffect(() => {
     const getdata = async () => {
-      const response = await axios.get(
+      const response = await fetch(
         `http://localhost:8000/api/v2/courses/${id}/`
       );
-      setCourse(response.data);
+      const result = await response.json();
+      setCourse(result);
     };
 
     getdata();
-  }, []);
+
+    return () => {};
+  }, [id]);
 
   return (
     <Container>
       <Card>
-        <Card.Header>{course.title}</Card.Header>
+        <Card.Header>{course ? course.title : <p>Sem dados</p>}</Card.Header>
         <Card.Body>
           <Card.Title></Card.Title>
           <div className="d-grid gap-2"></div>
-          <div className="d-grid gap-2">
-            {course.ratings.map((item) => (
-              <Button>{item.name}</Button>
-            ))}
-          </div>
+          <div className="d-grid gap-2"></div>
+          {course.ratings ? (
+            course.ratings.map((item) => <input defaultValue={item.comment} />)
+          ) : (
+            <p>Sem dados</p>
+          )}
         </Card.Body>
       </Card>
     </Container>
